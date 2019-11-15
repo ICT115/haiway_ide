9a01cb245fddef3ebd75b3edc5f75c86f1aa5290
bnyf <liyufeng614@126.com>
2019-11-15 23:40:11 +0800

    deploy on server

--- haiway_ide/settings.py
+++ haiway_ide/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=0%bx3!fbyhx*7eg!(r+o8*$#$6zi8sti(1131zh#j5ac^tr8h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,8 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'HTMLView')
-# STATIC_ROOT = os.path.join(STATIC_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'collectstatic')
 STATICFILES_DIRS = [
-    os.path.join(STATIC_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'HTMLView/static'),
 ]
